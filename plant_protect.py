5e187c7ee74eb56d6f0980e966b0eeb95e2aeaec
jkozma78 <jkozma@freestart.hu>
2022-05-17 19:03:25 +0200

    developing

--- plant_protect.py
+++ plant_protect.py
@@ -6,15 +6,12 @@ pp = Flask(__name__)
 
 @pp.route('/')
 def home():
-    # items = []
-    # for i in range(1, 111):
-    #     i = str(i)
-    #
-    #
-    #     an_item = dict(id=i, date="2012-02-24", chemical="here", plant="waiting", area="fólia", comment="")
-    #     items.append(an_item)
+    return render_template('home.html')
 
-    return render_template('plant2.html', items=items)
+
+@pp.route('/index')
+def index():
+    return render_template('plant.html', items=items)
 
 
 if __name__ == "__main__":
