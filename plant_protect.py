4b7d84ae0d7b88c50ef1b0d0afdc09b53f42fa9d
jkozma78 <jkozma@freestart.hu>
2023-01-08 17:30:28 +0100

    developing

--- plant_protect.py
+++ plant_protect.py
@@ -36,5 +36,5 @@ def index():
 
 
 if __name__ == "__main__":
-    pp.run(debug=True)
-    #pp.run(host='0.0.0.0', port=5001, debug=True)
+    pp.run(port=5000, debug=True)
+    #pp.run(host='127.0.0.1', port=5001, debug=True)
